cdab3af79da391690f445231371347550ecf6f08
zouhair.ettarak <ettarak.zouhair@gmail.com>
2023-06-16 20:28:34 +0100

    Break Down Url - Remove(www) and replace dash with - and split table

---
+++ BreakDownURL/BreakDownURL/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace BreakDownURL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "www.google.com/BreakDownUrl/todo-apps";
+
+            var components = BreakDowURL(url);
+            for(int i=0; i< components.Length; i++)
+            {
+                var indent = new string(' ', i * 2);
+                Console.WriteLine($"{indent} -> {components[i]}");
+            }
+            
+            
+            Console.ReadKey();
+        }
+
+        public static string[] BreakDowURL(string urlString)
+        {
+            // remove wwww
+            string noWWW = urlString.Remove(0, 4);
+            // NoDash
+            string noDashs= noWWW.Replace("-", "");
+            // split 
+            string[] components = noDashs.Split('/');
+            // alternative
+            //string[] x = url.Remove(0, 4).Replace("-", " ").Split('/');
+            return components;
+        }
+    }
+}
