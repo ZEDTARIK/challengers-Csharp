6f562a3220c0c73ecb10de143e348e45ab1eb511
zouhair.ettarak <ettarak.zouhair@gmail.com>
2023-06-21 21:39:56 +0100

    challenger Vowel Count

---
+++ VowelCount/VowelCount/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VowelCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // MARK: Setup
+            Console.WriteLine("Type in a word or phrase to find it's vowel score:");
+            string input = Console.ReadLine().ToLower();
+
+            // MARK: Result
+            Console.WriteLine($"Score: {VowelCount(input)}");
+            Console.ReadKey();
+        }
+
+        public static Dictionary<char, int> vowelScores = new Dictionary<char, int>()
+        {
+            { 'a', 1 },
+            { 'e', 2 },
+            { 'i', 3 },
+            { 'o', 4 },
+            { 'u', 5 },
+            { 'y', 9 }
+        };
+
+        public static int VowelCount(string text)
+        {
+            int totalScore = 0;
+            for(int i=0; i<text.Length; i++)
+            {
+                int points = 0;
+                if (vowelScores.TryGetValue(text[i], out points))
+                {
+                    totalScore += points;
+                }
+            }
+            return totalScore;
+        }
+    }
+}
