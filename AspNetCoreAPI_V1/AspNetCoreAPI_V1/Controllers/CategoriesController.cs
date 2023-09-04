a1a92f3a07334b8d5a18b80d5e1f140124e6f26d
zouhair.ettarak <ettarak.zouhair@gmail.com>
2023-09-04 15:29:05 +0100

    add new project asp net core API

---
+++ AspNetCoreAPI_V1/AspNetCoreAPI_V1/Controllers/CategoriesController.cs
@@ -0,0 +1,28 @@
+﻿using AspNetCoreAPI_V1.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreAPI_V1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+
+        public CategoriesController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> getCategories()
+        {
+            var categories= await _db.Categories.ToListAsync();
+            return Ok(categories);
+        }
+    }
+
+
+}
---
+++ AspNetCoreAPI_V1/AspNetCoreAPI_V1/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using AspNetCoreAPI_V1.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreAPI_V1.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
