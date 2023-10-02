501250d71bf4d328daae16565fc416ed71130039
zouhair.ettarak <ettarak.zouhair@gmail.com>
2023-10-02 14:44:45 +0100

    Sorted Alphabetique

---
+++ AlphabetSoup/AlphabetSoup/Program.cs
@@ -0,0 +1,20 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+
+String AlphabetSoup(String str)
+{
+    String result = string.Empty;
+
+    char[] charArray = str.ToLower().ToCharArray();
+    Array.Sort(charArray);
+
+    result = new string(charArray);
+
+    return result;
+
+}
+
+String result = AlphabetSoup("hello");// ehllo
+
+Console.WriteLine(result);
+Console.ReadLine();
