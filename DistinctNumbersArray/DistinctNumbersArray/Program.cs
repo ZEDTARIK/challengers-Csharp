51fa10c89bc0e48576374ff5e1f9d9fb2db50ec7
zouhair.ettarak <ettarak.zouhair@gmail.com>
2023-10-04 21:59:57 +0100

    Distinct Values from Arrayusing Distinct function

---
+++ DistinctNumbersArray/DistinctNumbersArray/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistinctNumbersArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 3, 4, 4, 5,6  };
+
+            var uniqueValue = numbers.Distinct().ToList();
+            //Console.WriteLine(distinctNumbers);
+
+            foreach (var number in uniqueValue)
+            {
+                Console.Write($"{number} ");
+            }
+            Console.ReadLine();
+
+            
+        }
+    }
+}
