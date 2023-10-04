4e32e3e5bf5b2f33ba6eeaa5d0948aec69489f92
zouhair.ettarak <ettarak.zouhair@gmail.com>
2023-10-04 15:37:16 +0100

    Get Address IP

---
+++ GetAdressIP/GetAdressIP/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetAdressIP
+{
+    internal class Program
+    {
+        [Obsolete]
+        static void Main(string[] args)
+        { 
+            // Récupérer le nom de l'hôte
+            string host = Dns.GetHostName();
+            Console.WriteLine("Le nom de l'hôte est :" + host);
+
+            // Récupérer l'adresse IP
+            string ip = Dns.GetHostByName(host).AddressList[0].ToString();
+            Console.WriteLine("Mon adresse IP est :" + ip);
+
+            Console.ReadLine();
+        }
+    }
+}
