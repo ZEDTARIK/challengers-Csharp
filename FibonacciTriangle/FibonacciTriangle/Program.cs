2b66a3dee76938e66f602f106c4e6d9c602240c1
zouhair.ettarak <ettarak.zouhair@gmail.com>
2023-10-20 11:37:55 +0100

    Fibonnaci with Triangle

---
+++ FibonacciTriangle/FibonacciTriangle/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciTriangle
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a=0, b=1, i,c,n,j;
+
+            Console.WriteLine("Enter The Limit of traingle of fibonacci: ");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("the Result is :\n");
+
+            for(i=1; i<= n; i++)
+            {
+                a = 0;
+                b = 1;
+                Console.Write(b + "\t");
+
+                for(j=1; j<i; j++)
+                {
+                    c = a + b;
+                    Console.Write(c + "\t");
+                    a = b;
+                    b = c;
+
+                }
+
+                Console.Write('\n');
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
